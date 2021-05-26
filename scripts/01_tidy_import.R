09151dd87dc6c7467a18c3b66096746dedd6d51a
ntlee1 <53658810+ntlee1@users.noreply.github.com>
2021-05-26 15:55:45 -0500

    Major Refactor

--- scripts/01_tidy_import.R
+++ scripts/01_tidy_import.R
@@ -16,27 +16,28 @@ pkgReq <- c("tidyverse",
             "rlist",
             "readxl",
             "stringr",
-            "tidytext")
+            "tidytext",
+            "stats")
 lapply(pkgReq, require, character.only = TRUE)
 
 #Import Data -------------------------------------------------------------------
 kikstrt <- readr::read_csv(here::here("data", "Kickstarter Data", "ksprojects.csv"))
-#print(kikstrt, tibble.width = "inf")
 kikstrt <- as_tibble(kikstrt)
 colnames(kikstrt)
+lapply(kikstrt, class)
+#Convert select cols to factor for easier manipulation
+kikstrt$category <- as.factor(kikstrt$category)
+kikstrt$main_category <- as.factor(kikstrt$main_category)
+kikstrt$currency <- as.factor(kikstrt$currency)
+kikstrt$state <- as.factor(kikstrt$state)
+kikstrt$country <- as.factor(kikstrt$country)
 
-#Reclassify state == undefined ---------------------------------------------------
-#When state == undefined, I noticed usd pledged is NA in every observation 
-#It appears the API relies on state being defined to give usd pledged a value 
-#We can still infer campaign success by usd_pledged_real >= usd_goal_real
-unique(kikstrt$state)
-stateUndef <- (filter(kikstrt, state == "undefined"))
-sum(is.na(stateUndef$`usd pledged`)) == nrow(stateUndef)
-stateUndefSuccess <- (filter(kikstrt, state == "undefined" & usd_pledged_real >= usd_goal_real))
-stateUndefFail <- (filter(kikstrt, state == "undefined" & usd_pledged_real <= usd_goal_real))
-kikstrt <- rbind(kikstrt, stateUndefSuccess, stateUndefFail)
+#Cleanse and Check Each Column for Errors --------------------------------------
+#NAs ---------------------------------------------------------------------------
+#name, usd_pledged have NAs.
+map(kikstrt, ~sum(is.na(.)))
 
-#Cleanse NAs -------------------------------------------------------------------
+#Find row position of NAs 
 naVals <- 1:ncol(kikstrt)
 naOut <- vector("list", length(naVals))
 naColNms <- vector("list", length(naVals))
@@ -54,9 +55,39 @@ kikstrt[naOut$name,]$name <- "NONAME"
 #view(kikstrt[naOut$`usd pledged`,])
 usdPlgNaRows <- naOut$`usd pledged`
 
-#Fix misleading deadline dates -------------------------------------------------
-#Some launch dates are misformatted as 1970-01-01
-#This is not a valid kickstarter was founded in 2009
+#Fix misclassified state values ------------------------------------------------
+#When state == undefined, I noticed usd pledged is NA in every observation 
+#It appears the API relies on state being defined to give usd pledged a value 
+#We can still infer campaign success by usd_pledged_real >= usd_goal_real
+unique(kikstrt$state)
+stateUndef <- (filter(kikstrt, state == "undefined"))
+sum(is.na(stateUndef$`usd pledged`)) == nrow(stateUndef)
+stateUndefSuccess <- (filter(kikstrt, state == "undefined" & usd_pledged_real >= usd_goal_real))
+stateUndefFail <- (filter(kikstrt, state == "undefined" & usd_pledged_real <= usd_goal_real))
+kikstrt <- rbind(kikstrt, stateUndefSuccess, stateUndefFail)
+
+#Remove duplicate rows. ~3400 found --------------------------------------------
+length(kikstrt$ID) == length(unique(kikstrt$ID))
+duplicateId <- table(kikstrt$ID) %>%
+  data.frame
+duplicateId <- filter(duplicateId, 1 < duplicateId$Freq)
+duplicateIdKik <- filter(kikstrt, ID %in% duplicateId$Var1)
+table(duplicateId$Freq)
+#Remove duplicate data
+kikstrt <- kikstrt %>% 
+  distinct
+#TRUE
+length(kikstrt$ID) == length(unique(kikstrt$ID))
+
+#Check category, main_category, currency for case sensitive duplication --------
+#Visual inspection, no issues found
+unique(kikstrt$category) 
+unique(kikstrt$main_category) 
+unique(kikstrt$currency)
+
+#Fix incorrect deadline dates --------------------------------------------------
+#~15 launch dates are misformatted as 1970-01-01
+#This is not a valid. Kickstarter was founded in 2009
 #It appears the web scraping API defaulted to the POSIX start date for an unknown reason
 #For practical purposes I have set the launch date 1 month before deadline
 mod1970 <- (filter(kikstrt, launched < "1971-01-01"))
@@ -65,6 +96,80 @@ kikstrt <- full_join(mod1970, kikstrt)
 #Append modifications then filter out erroneous data
 kikstrt <- filter(kikstrt, launched > "1971-01-01")
 
+#Remove timestamp from launched for easy manipulation
+kikstrt$launched <- as.Date(kikstrt$launched, format = "%Y-%m-%d")
+
+
+#Kickstarter DOES NOT provide accurate usd pledged data. Fixer.io does. -------- 
+#Reasoning:
+#I suspect data scraping errors in fx conversion columns usd_pledged and usd_pledged_real
+#Logically, if a project is in base currency USD,
+#columns "pledged", "usd_pledged", and "usd_pledged_real" should be equivalent.
+#They all measure the same metric, total USD dollars pledged
+#This is not always the case. There are major discrepancies. 
+#For simplicity, lets analyze USD projects as the FX conversion is 1:1
+#TODO MAYBE check NAs for other currencies and import external data for use
+
+#Pledge Accuracy: Kickstarter vs Fixer.io
+#Filter all projects with base USD currency
+#297,990 USD Projects
+baseUsd <- filter(kikstrt, `currency` == "USD")
+numberUSDProjects <- nrow(baseUsd)
+
+#Create Kickstarter accuracy ratio for usd_pledged
+pledgeUSDKik <- baseUsd$pledged == baseUsd$`usd pledged`
+#5363 NAs detected
+naUSDKik <- sum(is.na(pledgeUSDKik))
+#Kickstarter provides the correct USD FX conversion 85% of the time for USD projects
+pledgeUSDKikAccRatio <- sum(pledgeUSDKik, na.rm = TRUE)/(numberUSDProjects - naUSDKik)
+
+#Create fixer.io accuracy ratio for usd_pledged_real
+pledgeUSDFixer <- baseUsd$pledged == baseUsd$usd_pledged_real
+#No NAs detected from fixer.io
+naUSDFixer <- sum(is.na(pledgeUSDFixer))
+#Fixer.io provides the correct USD FX conversion 100% of the time for USD projects
+#Added benefit of zero NAs
+pledgeUSDFixerAccRatio <- sum(pledgeUSDFixer)/numberUSDProjects
+
+#Can we rely on Fixer.io to provide 100% accuracy for other denominations?
+#Validation Process: 
+#1. Import external USD FX conversion data for time period 
+#2. Convert pledged column manually and compare to fixer.io 
+#3. If relatively similar then fixer.io is assumed accurate 
+unique(kikstrt$currency)
+
+#Step1: 
+#Import FX data from https://www.ofx.com/en-us/forex-news/historical-exchange-rates/
+#Note that FX data will differ because exact time of day is taken into account
+#YMD 
+range(kikstrt$deadline)
+
+#Test fixer.io accuracy for USDAUD to verify accuracy with a non-USD denomination
+ofxUsdAud <- readxl::read_xlsx(here::here("data", "ofx_fx_data", "USDAUD_ofx.xlsx"))
+baseAud <- filter(kikstrt, `currency` == "AUD")
+#9581 AUD projects
+nrow(baseAud)
+
+#Match dates of USDAUD external FX rate and multiply pledged by currency value
+ofxUsdAud$deadline <- ofxUsdAud$Date
+ofxUsdAud$deadline <- as.Date(ofxUsdAud$deadline, format = "Y-m-d")
+class(ofxUsdAud$deadline)
+baseAud <- right_join(baseAud, ofxUsdAud, by = "deadline")
+baseAud$ofx_usd_pledged <- baseAud$`pledged`/baseAud$USDAUD
+
+#Accuracy ranges from factor of 0.86 to 1.04, variance 0.00012
+#Reasonable numbers. Low spread, low variance
+#Fixer.io is highly accurate compared to ofx_usd_pledged column.
+#Kickstarter is not even close
+ofxPledgeCompare <- baseAud$usd_pledged_real/baseAud$ofx_usd_pledged
+#1715 NAs detected. These are projects with zero money raised. Remove
+sum(is.na(baseAud$usd_pledged_real))
+ofxPledgeCompare <- na.omit(ofxPledgeCompare)
+range(ofxPledgeCompare)
+var(ofxPledgeCompare)
+
+#Drop usd pledged for inaccuracy
+kikstrt <- select(kikstrt, -c(`usd pledged`))
 
 
 
@@ -87,3 +192,22 @@ kikstrt <- filter(kikstrt, launched > "1971-01-01")
 
 
 
+#Create 4 categories of project size using quantiles ---------------------------
+#of usd_goal_real, state = success
+#Quantiles = 1302, 3838, 10000
+#Project Sizes: Small, Mid, Large, Premium
+kikSmall <- filter(kikstrt, usd_goal_real < 1302) 
+kikSmall$size <- "Small"
+kikMid <- filter(kikstrt, 1302 <= usd_goal_real & usd_goal_real < 3838)
+kikMid$size <- "Mid"
+kikLarge <- filter(kikstrt, 3838 <= usd_goal_real & usd_goal_real < 10000) 
+kikLarge$size <- "Large"
+kikPrem <- filter(kikstrt, 10000 <= usd_goal_real)
+kikPrem$size <- "Prem"
+#Add size classification
+kikstrt <- rbind(kikSmall, kikMid, kikLarge, kikPrem) %>%
+  data.frame
+
+#Add Length of Campaign --------------------------------------------------------
+kikstrt$projDays <- (kikstrt$deadline - kikstrt$launched) %>%
+  as.numeric
--- scripts/07_optimal_goal_amount.R
+++ scripts/07_optimal_goal_amount.R
@@ -7,12 +7,11 @@
 #Reason: We want to model realistic campaigns, not the breakout successes
 #Define breakout success as median of goalExceedRatio
 #Only analyze state == success
-
 breakoutFilter <- kikstrt
 breakoutFilter <- filter(breakoutFilter, state == "successful")
 breakoutFilter$goalExceedRatio <- breakoutFilter$usd_pledged_real/breakoutFilter$usd_goal_real
 breakoutFilterSummary <- summary(breakoutFilter$goalExceedRatio)
-view(breakoutFilter)
+#view(breakoutFilter)
 
 #Step 2: Find median for each project size classification
 #Success Quantiles 1302, 3838, 10000
@@ -34,73 +33,194 @@ breakoutLargeMedian <- summary(breakoutLarge$goalExceedRatio)[3]  %>%
 breakoutPremMedian <- summary(breakoutPrem$goalExceedRatio)[3]  %>%
   as.numeric
 
+breakoutSmall <- filter(breakoutSmall, goalExceedRatio <= breakoutSmallMedian)
+breakoutSmall$Size <- "Small"
+breakoutMid <- filter(breakoutMid, goalExceedRatio <= breakoutMidMedian)
+breakoutMid$Size <- "Mid"
+breakoutLarge <- filter(breakoutLarge, goalExceedRatio <= breakoutLargeMedian)
+breakoutLarge$Size <- "Large"
+breakoutPrem <- filter(breakoutPrem, goalExceedRatio <= breakoutPremMedian)
+breakoutPrem$Size <- "Prem"
 
+breakoutFilterDf <- rbind(breakoutSmall,
+                          breakoutMid,
+                          breakoutLarge,
+                          breakoutPrem) %>%
+  as.data.frame
 
-
-
-#Step 3: Create function
+#Step 3: Estimate the average number of backers, and their estimated contribution
+#per backer for a given goal amount
 #Goal amount will be used to classify project size and select appropriate
 #goalExceedRatio to define breakout success
-#If goal range exceeds limits of size classification, then the additional points 
+#If goal range exceeds limits of size classification, then the additional points =
 #will be used and not excluded
 #Range will be defined as +- 20% goalExceedRatio
 
-#Inputs: 
-function() {
+#Inputs: myGoalAmount
+goalAmountFunction <- function(myGoalAmount) {
+  if(myGoalAmount < 1302) {
+    myRange <-  filter(breakoutFilterDf, myGoalAmount*0.8 <= usd_goal_real, usd_goal_real <= myGoalAmount*1.2) 
+    backerAvgPledge <- (mean(myRange$usd_pledged_real)/mean(myRange$backers)) %>%
+      round(., digits = 2)
+    backerAvgCount <- mean(myRange$backers) %>%
+      round(., digits = 0)
+    paste("For a kickstarter goal of", paste0("$", myGoalAmount, ","),
+          "the average number of backers is", backerAvgCount,
+          "and the average contribution per backer is",
+          paste0("$", backerAvgPledge, "."))
+  } else {
+    if(1302 <= myGoalAmount & myGoalAmount < 3838) {
+      myRange <-  filter(breakoutFilterDf, myGoalAmount*0.8 <= usd_goal_real, usd_goal_real <= myGoalAmount*1.2) 
   
+      backerAvgPledge <- (mean(myRange$usd_pledged_real)/mean(myRange$backers)) %>%
+        round(., digits = 2)
+      backerAvgCount <- mean(myRange$backers) %>%
+        round(., digits = 0)
+      paste("For a kickstarter goal of", paste0("$", myGoalAmount, ","),
+            "the average number of backers is", backerAvgCount,
+            "and the average contribution per backer is",
+            paste0("$", backerAvgPledge, "."))
+    } else {
+      if(3838 <= myGoalAmount & myGoalAmount < 10000) {
+        myRange <-  filter(breakoutFilterDf, myGoalAmount*0.8 <= usd_goal_real, usd_goal_real <= myGoalAmount*1.2) 
+       
+        backerAvgPledge <- (mean(myRange$usd_pledged_real)/mean(myRange$backers)) %>%
+          round(., digits = 2)
+        backerAvgCount <- mean(myRange$backers) %>%
+          round(., digits = 0)
+        paste("For a kickstarter goal of", paste0("$", myGoalAmount, ","),
+              "the average number of backers is",
+              backerAvgCount, "and the average contribution per backer is",
+              paste0("$", backerAvgPledge, ".")) 
+      } else {
+        if(10000 <= myGoalAmount) {
+          myRange <-  filter(breakoutFilterDf, myGoalAmount*0.8 <= usd_goal_real, usd_goal_real <= myGoalAmount*1.2) 
+          
+          backerAvgPledge <- (mean(myRange$usd_pledged_real)/mean(myRange$backers)) %>%
+            round(., digits = 2)
+          backerAvgCount <- mean(myRange$backers) %>%
+            round(., digits = 0)
+          paste("For a kickstarter goal of", paste0("$", myGoalAmount, ","),
+                "the average number of backers is", backerAvgCount,
+                "and the average contribution per backer is",
+                paste0("$", backerAvgPledge, ".")) 
+        }
+      }
+    }
+  }
+}
+
+#Plot myGoalAmount ----------
+#Enable function to give me outputs needed to create plot
+goalAmountFunctionPlot <- function(myGoalAmount) {
+  if(myGoalAmount < 1302) {
+    myRange <-  filter(breakoutFilterDf, myGoalAmount*0.8 <= usd_goal_real, usd_goal_real <= myGoalAmount*1.2) 
+    backerAvgPledge <- (mean(myRange$usd_pledged_real)/mean(myRange$backers)) %>%
+      round(., digits = 2)
+    backerAvgCount <- mean(myRange$backers) %>%
+      round(., digits = 0)
+    myDf <- data.frame(x = backerAvgPledge,
+                       x1 = backerAvgCount)
+    colnames(myDf) <- c("backerAvgPledge", "backerAvgCount")
+    myDf
+  } else {
+    if(1302 <= myGoalAmount & myGoalAmount < 3838) {
+      myRange <-  filter(breakoutFilterDf, myGoalAmount*0.8 <= usd_goal_real, usd_goal_real <= myGoalAmount*1.2) 
+      backerAvgPledge <- (mean(myRange$usd_pledged_real)/mean(myRange$backers)) %>%
+        round(., digits = 2)
+      backerAvgCount <- mean(myRange$backers) %>%
+        round(., digits = 0)
+      myDf <- data.frame(x = backerAvgPledge,
+                         x1 = backerAvgCount)
+      colnames(myDf) <- c("backerAvgPledge", "backerAvgCount")
+      myDf
+    } else {
+      if(3838 <= myGoalAmount & myGoalAmount < 10000) {
+        myRange <-  filter(breakoutFilterDf, myGoalAmount*0.8 <= usd_goal_real, usd_goal_real <= myGoalAmount*1.2) 
+        backerAvgPledge <- (mean(myRange$usd_pledged_real)/mean(myRange$backers)) %>%
+          round(., digits = 2)
+        backerAvgCount <- mean(myRange$backers) %>%
+          round(., digits = 0)
+        myDf <- data.frame(x = backerAvgPledge,
+                           x1 = backerAvgCount)
+        colnames(myDf) <- c("backerAvgPledge", "backerAvgCount")
+        myDf
+      } else {
+        if(10000 <= myGoalAmount) {
+          myRange <-  filter(breakoutFilterDf, myGoalAmount*0.8 <= usd_goal_real, usd_goal_real <= myGoalAmount*1.2) 
+          backerAvgPledge <- (mean(myRange$usd_pledged_real)/mean(myRange$backers)) %>%
+            round(., digits = 2)
+          backerAvgCount <- mean(myRange$backers) %>%
+            round(., digits = 0)
+          myDf <- data.frame(x = backerAvgPledge,
+                             x1 = backerAvgCount)
+          colnames(myDf) <- c("backerAvgPledge", "backerAvgCount")
+          myDf
+        }
+      }
+    }
+  }
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-breakoutFilterSmall <- projSmall
-breakoutFilterSmall <- filter(breakoutFilterSmall, state == "successful")
-breakoutFilterSmall$goalExceedRatio <- breakoutFilterSmall$usd_pledged_real/breakoutFilterSmall$usd_goal_real
-
-breakoutFilterSmallSummary<- summary(breakoutFilterSmall$goalExceedRatio)
-breakoutFilterSmallIQR <- (breakoutFilterSmallSummary[5] -  breakoutFilterSmallSummary[2]) %>%
+#Create four different plots depending on the size of the project
+#Plot a subset of points then connect
+#Small seq(0,1302, by 100)
+#TODO Cut by an even number of points
+#TODO Fix variable names
+goalFunSeq <- seq(1, 700000, by = 100) %>%
+  data.frame
+colnames(goalFunSeq) <- c("myGoalAmount")
+goalFunMap <- pmap(goalFunSeq, goalAmountFunctionPlot) %>%
+  unlist %>%
   as.numeric
 
-breakoutFilterSmall <- filter(breakoutFilterSmall, goalExceedRatio <=  breakoutFilterSmallIQR)
+length(goalFunMap)
+#Average Pledge
+
+goalFunSeqData <- data.frame(x = goalFunMap[seq(1, 14000, by = 2)],
+                           x1 = goalFunMap[seq(0, 14000, by = 2)[-1]])
+goalFunSeqData$GoalAmount <- seq(1, 700000, by = 100)
+colnames(goalFunSeqData) <- c("BackerAvgPledge", "BackerAvgCount", "GoalAmount")
 
 
-#Create function 
-#Inputs: Goal Range, Category
-#Outputs: Estimated number of backers, estimated dollars per backer
-#Success Quantiles 1302, 3838, 10000
-catNmsBreakout <- unique(categorySummaryCount$main_category) %>%
-  data.frame
-colnames(catNmsBreakout) <- c("Category")
+#The more backers a project has, the more money they contribute??
+#This could make sense because successful projects need to meet 100% of their goal
+#in order to be funded
+#So large projects need more backers who contribute increasing amounts of more money
+#One would think that per contribution numbers would be more stagnant
+
+ggplot2::ggplot(goalFunSeqData, aes(x = BackerAvgCount, y = BackerAvgPledge)) +
+  geom_point()
+
+ggplot2::ggplot(goalFunSeqData, aes(y = BackerAvgCount, x = GoalAmount)) +
+  geom_point()
+
+ggplot2::ggplot(goalFunSeqData, aes(y = BackerAvgPledge, x = GoalAmount)) +
+  geom_point()
 
 
-test <- filter(breakoutFilterSmall, 800 <= usd_goal_real, usd_goal_real <= 1000,
-       category == catNmsBreakout[1,])
-testMean <- mean(test$backers)
-testRaiseAvg <- mean(test$usd_pledged_real)
-testDollarsPerBacker <- testRaiseAvg/testMean
+##################
+#ITS WRONG BECAUSE I HAVENT FILTERED OUT THE EXTREME SMALL AND LARGE PROJECTS
 
 
-function(goalAmount, catIndex) {
-  if(goalAmount < 1302) {
-    mySmallBreakoutFilter <- filter(breakoutFilterSmall,
-                                    goalAmount*0.8 <= usd_goal_real,
-                                    usd_goal_real <= goalAmount*1.2,
-                   category == catNmsBreakout[catIndex,])
-    myMean <- mean(test$backers)
-    testRaiseAvg <- mean(test$usd_pledged_real)
-    testDollarsPerBacker <- testRaiseAvg/myMean
-  }
 
-  
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
